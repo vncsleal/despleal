28930406c4c005797b5b067b27ac4e2b89650a63
Vinicius Leal <viniciusleal.s@gmail.com>
2023-08-25 18:35:41 -0300

    add comment to index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,31 +6,38 @@ import { createClient } from "@/prismicio";
 import { components } from "@/slices/";
 import { Layout } from "@/components/Layout";
 
+// Create a new instance of the Index component
 const Index = ({ page, navigation, settings }) => {
-  return (
-    <Layout navigation={navigation} settings={settings}>
-      <Head>
-        <title>{prismic.asText(page.data.title)}</title>
-      </Head>
-      <SliceZone slices={page.data.slices} components={components} />
-    </Layout>
-  );
+	// Return the Layout component with the navigation and settings props
+	return (
+		<Layout navigation={navigation} settings={settings}>
+			<Head>
+				<title>{prismic.asText(page.data.title)}</title>
+			</Head>
+			<SliceZone slices={page.data.slices} components={components} />
+		</Layout>
+	);
 };
 
+// Export the Index component as a static page
 export default Index;
 
+// Get the page, navigation and settings from the server
 export async function getStaticProps({ locale, previewData }) {
-  const client = createClient({ previewData });
+	// Create a new instance of the Prismic client
+	const client = createClient({ previewData });
 
-  const page = await client.getByUID("page", "home", { lang: locale });
-  const navigation = await client.getSingle("navigation", { lang: locale });
-  const settings = await client.getSingle("settings", { lang: locale });
+	// Get the page, navigation and settings from the server
+	const page = await client.getByUID("page", "home", { lang: locale });
+	const navigation = await client.getSingle("navigation", { lang: locale });
+	const settings = await client.getSingle("settings", { lang: locale });
 
-  return {
-    props: {
-      page,
-      navigation,
-      settings,
-    },
-  };
+	// Return the props for the static page
+	return {
+		props: {
+			page,
+			navigation,
+			settings,
+		},
+	};
 }
